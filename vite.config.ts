b3a3b1ea7ac4ed5df2a8efaa7b0acb162955a089
Piotrek Witkowski <piotrekwitkowski1@gmail.com>
2023-02-16 11:22:03 +0000

    Cleanup in vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,7 @@
-
-
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 
-export default defineConfig(({ command }) => ({
+export default defineConfig(() => ({
   plugins: [react()],
   base: '/',
   // test: {
