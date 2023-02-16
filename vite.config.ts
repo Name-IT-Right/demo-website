02a1d5ddee0bede03268d6d01e8d85651739b897
Piotrek Witkowski <piotrekwitkowski1@gmail.com>
2023-02-16 13:39:39 +0000

    Rename repo on Github to demo-website

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import react from "@vitejs/plugin-react-swc";
 export default defineConfig(({ command }) => ({
   plugins: [react()],
   // change base for Github Pages hosting on production builds
-  base: command === 'build' ? '/website/' : '/',
+  base: command === 'build' ? '/demo-website/' : '/',
   // test: {
   //   globals: true,
   //   environment: 'jsdom',
