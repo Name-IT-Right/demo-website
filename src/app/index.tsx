8e9cb2e36eeab175bb9c815008e17559352dc823
Piotrek Witkowski <piotrekwitkowski1@gmail.com>
2023-02-17 13:47:23 +0000

    Use HashRouter again

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,9 +1,9 @@
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import { createHashRouter, RouterProvider } from "react-router-dom";
 import { Home } from "../home";
 import { S3Demo } from "../s3";
 import { SagemakerDemo } from "../sagemaker";
 
-const router = createBrowserRouter([
+const router = createHashRouter([
   { path: "/", element: <Home /> },
   { path: "/s3", element: <S3Demo /> },
   { path: "/sagemaker", element: <SagemakerDemo /> },
